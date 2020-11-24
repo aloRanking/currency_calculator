ca6dec89cba42a26bd115c4b04dbdd012a737e4f
aloRanking <adigunlateef02@gmail.com>
2020-11-24 17:57:03 +0100

    added class for dropdown widget

---
+++ lib/models/currencyDropdown.dart
@@ -0,0 +1,16 @@
+class CurrencyDropDown{
+
+  String currencyName;
+
+  CurrencyDropDown({this.currencyName});
+
+
+}
+
+List<CurrencyDropDown> currency = [
+  CurrencyDropDown(currencyName: 'EUR'),
+  CurrencyDropDown(currencyName: 'USD'),
+  CurrencyDropDown(currencyName: 'MXM'),
+  CurrencyDropDown(currencyName: 'CAD')
+];
+
